81648bcab0a771d92ebeed1bd245da830da13f04
Development JavaScript <161371025+npmdevelopment@users.noreply.github.com>
2024-10-10 22:49:21 -0300

    Create main.js

---
+++ versions/1.0.0/main.js
@@ -0,0 +1,5 @@
+// Import the SecurityCredentials class
+import SecurityCredentials from "./security-credentials.js"; // Ensure to add .js for ES module
+
+// Export the class for external use
+export { SecurityCredentials };
